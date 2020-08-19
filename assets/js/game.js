60523044404e94a66ff57d4e41f5ea108d6b7319
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-18 18:23:02 -0600

    random

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,14 +23,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -49,7 +51,9 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -77,7 +81,8 @@ var startGame = function () {
     }
     var pickedEnemyName = enemyNames[i];
     //reset enemyHealth 
-    enemyHealth = 50;
+    // enemyHealth = Math.floor(Math.random() * 21) + 40;
+    enemyHealth = randomNumber(40, 60);
     // call fight function with enemy robot
     fight(pickedEnemyName);
     if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -147,13 +152,18 @@ var shop = function () {
       shop();
       break;
   }
-  default:
-window.alert("You did not pick a valid option. Try again.");
 
-// call shop() again to force player to pick a valid option
-shop();
-break;
+  // call shop() again to force player to pick a valid option
+  // shop();
 
+  // break;
+
+};
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 startGame();
